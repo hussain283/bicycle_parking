a53ef3ae5af36c44fb759b3ec6ac684d8f47cc90
Hussain Tambawala <hussain283@gmail.com>
2013-08-18 14:49:18 -0700

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BicycleParking::Application.config.secret_token = 'aec8460a5f82165bcaab9a2de8774514dba2a17e5e91fbdfe4b1df7fd2944d09c421127d0dafb761c7076d54c7dc3784967479bac40d6726a923dc904c4a59a2'
