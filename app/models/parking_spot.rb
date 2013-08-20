f78dc671f25652fbebdf714a0b666808e5a140fb
Hussain Tambawala <hussain283@gmail.com>
2013-08-19 23:13:50 -0700

    Fixed radius and return value

--- app/models/parking_spot.rb
+++ app/models/parking_spot.rb
@@ -7,8 +7,8 @@ class ParkingSpot < ActiveRecord::Base
 
   def self.search(query)
     coordinates = Geocoder.coordinates(query);
-    p self.near(coordinates,0.1);
-    {center: coordinates, parkingSpots: self.near(coordinates,0.1)}
+    p self.near(coordinates,0.2);
+    {center: coordinates, parkingSpots: self.near(coordinates,0.2).limit(5)}
   end
 
   def full_address
