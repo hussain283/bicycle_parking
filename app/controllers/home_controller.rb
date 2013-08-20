dc51a50d067425b4a8251f257457b6e9c6ef2fec
Hussain Tambawala <hussain283@gmail.com>
2013-08-19 22:42:33 -0700

    GeoLocation

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,6 @@ class HomeController < ApplicationController
   respond_to :html, :json
   
   def index
-    ip_address = '2662 22nd St, San Francisco, CA'
-    respond_with(@parking_spots = ParkingSpot.search(ip_address).to_json);
+    respond_with(@parking_spots = ParkingSpot.search(request.location).to_json);
   end
 end
