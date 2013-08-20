f1421e06132a456d13c62c4ba262912de5baa594
Hussain Tambawala <hussain283@gmail.com>
2013-08-20 00:03:19 -0700

    Error catching

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  unless Rails.application.config.consider_all_requests_local
+    rescue_from ActionController::RoutingError, ActionController::UnknownController, ::AbstractController::ActionNotFound, ActiveRecord::RecordNotFound, :with => :error_handler
+  end
+
+  def error_handler
+    redirect_to root_url
+  end
 end
